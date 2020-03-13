250cd2716811d57ca0f646fb982ee87c2f9dbab7
equidistant <mich.random@pm.me>
2020-03-13 20:36:02 +0100

    removed social networks from home, added links

--- src/components/composite/Home.js
+++ src/components/composite/Home.js
@@ -1,23 +1,32 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import styled from 'styled-components'
 import { RippleComponent } from '../../hooks'
 import { CoverImg } from '../../images'
 import { FacebookImg, InstagramImg } from '../../images'
-import { IconButton } from '../basic'
+import { H3 } from '../basic'
 import theme from '../../theme'
 import { externalTo } from '../../utils'
 
 const Component = () => {
+  const history = useHistory()
   return (
     <Root>
       <Header>The world through my lens.</Header>
-      <IconContainer>
-        <RippleComponent Component={IconButton} url={FacebookImg} rippleColor={'#FFFFFF7C'} round={true} onClick={() => externalTo('https://www.facebook.com/littleeaglephoto')}/>
-        <RippleComponent Component={IconButton}  url={InstagramImg} rippleColor={'#FFFFFF7C'} round={true} onClick={() => externalTo('https://www.instagram.com/littleeaglephoto')}/>
-      </IconContainer>
+      <LinkContainer>
+        <LinkLeft onClick={() => history.push('/gallery')}>Gallery</LinkLeft>
+        <Divider />
+        <LinkRight onClick={() => history.push('/about')}>About me</LinkRight>
+      </LinkContainer>
     </Root>
   )
 }
+
+// <IconContainer>
+//   <RippleComponent Component={IconButton} url={FacebookImg} rippleColor={'#FFFFFF7C'} round={true} onClick={() => externalTo('https://www.facebook.com/littleeaglephoto')}/>
+//   <RippleComponent Component={IconButton}  url={InstagramImg} rippleColor={'#FFFFFF7C'} round={true} onClick={() => externalTo('https://www.instagram.com/littleeaglephoto')}/>
+// </IconContainer>
+
 
 const Root = styled.div`
   background: linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.3)), url(${CoverImg});;
@@ -31,14 +40,50 @@ const Root = styled.div`
   margin-top: 64px;
 `
 
-const IconContainer = styled.div`
+const LinkContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  padding: ${theme.spacing(0.5)};
-  margin-right: ${theme.spacing(1)};
   margin-bottom: 64px;
 `
+
+const Link = styled.p`
+  color: white;
+  font-family: 'Raleway';
+  font-size: 30px;
+  cursor: pointer;
+  opacity: 0.7;
+  transition: opacity 0.2s ease-in-out;
+  &:hover {
+    opacity: 1;
+  }
+  &:active {
+    transform: translateY(2px);
+  }
+`
+
+const LinkLeft = styled(Link)`
+  margin-right: ${theme.spacing(3)};
+`
+
+const LinkRight = styled(Link)`
+  margin-left: ${theme.spacing(3)};
+`
+
+const Divider = styled.div`
+  height: 30px;
+  width: 0px;
+  border: 1px solid white;
+`
+
+// const IconContainer = styled.div`
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   padding: ${theme.spacing(0.5)};
+//   margin-right: ${theme.spacing(1)};
+//   margin-bottom: 64px;
+// `
 
 const Header = styled.p`
   font-size: 36px;
