79685216750e25b0655c1930a453329d43521710
equidistant <mich.random@pm.me>
2020-07-08 14:24:04 +0200

    readme update

---
+++ README.md
@@ -0,0 +1,26 @@
+## Little Eagle Photography
+
+Professional photography website.<br>
+Features custom made justified paginated swipeable gallery.
+All the images are in the src folder which is problematic.
+It's fixed in next version.
+
+### Live Preview
+
+n/a
+
+### Config
+
+Create .env file with following variables:
+
+PORT (optional, default is 3000)<br>
+REACT_APP_IMG_URL (mandatory, base url to http server with images)
+
+### `npm start`
+
+Runs app in development mode.<br>
+Default is [http://localhost:3000](http://localhost:3000).<br>
+
+### `npm run build`
+
+Creates an optimized production build.
--- src/images/index.js
+++ src/images/index.js
@@ -1,4 +1,4 @@
-const imgUrl = `${process.env.PUBLIC_URL}/img`
+const imgUrl = process.env.REACT_APP_IMG_URL
 
 export const LogoImg = `${imgUrl}/logo.png`
 export const LogoTextImg = `${imgUrl}/logo-text.png`
@@ -24,12 +24,14 @@ export const arrow = {
 export const X = `${imgUrl}/misc/x.svg`
 
 
-const imagePaths = ({ dir, subdir, length }) => [...Array(length)].map((img, index) => `${imgUrl}/${dir}/${subdir}/img${index}.jpg`)
+// const imagePaths = ({ dir, subdir, length }) => [...Array(length)].map((img, index) => `${imgUrl}/${dir}/${subdir}/img${index}.jpg`)
+
+const imagePaths = ({ dir, length }) => [...Array(length)].map((img, index) => `${imgUrl}/${dir}/img${index}.jpg`)
 
 export const galleryImages = {
-  'details': imagePaths({ dir: 'gallery', subdir: 'details', length: 32 }),
-  'people': imagePaths({ dir: 'gallery', subdir: 'people', length: 137 }),
-  'travels': imagePaths({ dir: 'gallery', subdir: 'travels', length: 163 }),
-  'chill-vibez-battles': imagePaths({ dir: 'gallery', subdir: 'projects/chill-vibez-battles', length: 101 }),
-  'supeus-case-study': imagePaths({ dir: 'gallery', subdir: 'projects/supeus-case-study', length: 26 }),
+  'details': imagePaths({ dir: 'details', length: 32 }),
+  'people': imagePaths({ dir: 'people', length: 137 }),
+  'travels': imagePaths({ dir: 'travels', length: 163 }),
+  'chill-vibez-battles': imagePaths({ dir: 'projects/chill-vibez-battles', length: 101 }),
+  'supeus-case-study': imagePaths({ dir: 'projects/supeus-case-study', length: 26 }),
 }
